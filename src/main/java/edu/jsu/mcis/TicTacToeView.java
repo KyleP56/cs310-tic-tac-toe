240759fd595245b3110afb66ccbb33581096f901
Kyle Phillips <49827886+KyleP56@users.noreply.github.com>
2019-09-29 01:35:08 -0500

    Part 2 Complete

---
+++ src/main/java/edu/jsu/mcis/TicTacToeView.java
@@ -0,0 +1,84 @@
+package edu.jsu.mcis;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.awt.event.*;
+
+public class TicTacToeView extends JPanel {
+    
+    private final TicTacToeController controller;
+
+    private final JButton[][] board;
+    private final JPanel squaresPanel;
+    private final JLabel resultLabel;
+
+    public TicTacToeView(TicTacToeController controller, int width) {
+
+        this.controller = controller;
+
+        this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        board = new JButton[width][width];
+        squaresPanel = new JPanel(new GridLayout(width,width));
+        resultLabel = new JLabel();
+        resultLabel.setName("ResultLabel");
+        
+        for (int row = 0; row < width; row++) {
+            
+            for (int col = 0; col < width; col++) {
+                
+                board[row][col] = new JButton(); 
+                board[row][col].addActionListener(controller);
+                board[row][col].setName("Square" + row + col);
+                board[row][col].setPreferredSize(new Dimension(64,64));
+                squaresPanel.add(board[row][col]);
+                
+            }
+            
+        }
+
+        this.add(squaresPanel);
+        this.add(resultLabel);
+        
+        resultLabel.setText("Welcome to Tic-Tac-Toe!");
+
+    }
+        
+    public void updateSquares() {
+
+        /* Refresh the GUI with updated data from the Model (via the Controller) */
+
+        // INSERT YOUR CODE HERE
+        for (int row = 0; row < controller.getWidth(); row++){
+            for (int col = 0; col < controller.getWidth(); col++){
+                board[row][col].setText(controller.getMark(x,y).toString());
+            }
+        }
+
+    }
+    
+    public void disableSquares() {
+
+        /* Disable buttons (to disallow input after game is over) */
+    
+        // INSERT YOUR CODE HERE
+        for (int row = 0; row < controller.getWidth(); row++){
+            for (int col = 0; col < controller.getWidth(); col++){
+                board[row][col].setEnabled(false);
+            }
+        }
+            
+    }
+        
+    public void showResult(String message) {
+        
+        resultLabel.setText(message);
+        
+    }
+    
+    public void clearResult() {
+        
+        resultLabel.setText(" ");
+        
+    }
+
+}
